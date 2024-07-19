28550f506a30280ad7f073afaf2af5a1d97f0fba
Denys Olleik <dolleik@gmail.com>
2024-07-19 04:39:56 -0500

    Adding some validation.

--- Accounting/Validators/CreateItemViewModelValidator.cs
+++ Accounting/Validators/CreateItemViewModelValidator.cs
@@ -17,23 +17,35 @@ namespace Accounting.Validators
           .MaximumLength(100).WithMessage("Name cannot be more than 100 characters.");
 
       RuleFor(x => x.SelectedRevenueChartOfAccountId)
-        .NotEmpty().WithMessage("Revenue account is required.").DependentRules(() =>
-        {
-          RuleFor(x => x.SelectedRevenueChartOfAccountId)
-            .MustAsync(async (chartOfAccountId, cancellation) => await AccountExists(chartOfAccountId, _organizationId, cancellation))
-            .WithMessage("Selected revenue account does not exist.");
-        });
+          .NotEmpty().WithMessage("Revenue account is required.")
+          .DependentRules(() =>
+          {
+            RuleFor(x => x.SelectedRevenueChartOfAccountId)
+                      .MustAsync(async (chartOfAccountId, cancellation) => await AccountExists(chartOfAccountId, _organizationId, cancellation))
+                      .WithMessage("Selected revenue account does not exist.");
+          });
 
       RuleFor(x => x.SelectedAssetsChartOfAccountId)
-        .NotEmpty().WithMessage("Asset account is required.").DependentRules(() =>
-        {
-          RuleFor(x => x.SelectedAssetsChartOfAccountId)
-            .MustAsync(async (chartOfAccountId, cancellation) => await AccountExists(chartOfAccountId, _organizationId, cancellation))
-            .WithMessage("Selected asset account does not exist.");
-        });
+          .NotEmpty().WithMessage("Asset account is required.")
+          .DependentRules(() =>
+          {
+            RuleFor(x => x.SelectedAssetsChartOfAccountId)
+                      .MustAsync(async (chartOfAccountId, cancellation) => await AccountExists(chartOfAccountId, _organizationId, cancellation))
+                      .WithMessage("Selected asset account does not exist.");
+          });
 
       RuleFor(x => x.SelectedItemType)
-        .NotEmpty().WithMessage("Item type is required.");
+          .NotEmpty().WithMessage("Item type is required.");
+
+      // Conditional validation for Quantity and SalePrice
+      When(x => x.SelectedLocationId.HasValue, () =>
+      {
+        RuleFor(x => x.Quantity)
+            .GreaterThan(0).WithMessage("Quantity is required and must be greater than zero.");
+
+        RuleFor(x => x.SalePrice)
+            .GreaterThan(0).WithMessage("Sale Price is required and must be greater than zero.");
+      });
     }
 
     private async Task<bool> AccountExists(int? chartOfAccountId, int organizationId, CancellationToken cancellationToken)
