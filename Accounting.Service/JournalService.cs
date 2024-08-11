666a7d5d1fb1e8c7d86f56f70e6c306598f914d5
Denys Olleik <dolleik@gmail.com>
2024-08-10 19:49:59 -0500

    Adding shipping and receiving navigation option.

---
+++ Accounting.Service/JournalService.cs
@@ -0,0 +1,25 @@
+﻿namespace Accounting.Service
+{
+  public class JournalService
+  {
+    public async Task RecordInvoiceAsync()
+    {
+
+    }
+
+    public async Task UpdateInvoiceAsync()
+    {
+
+    }
+
+    public async Task VoidInvoiceLineAsync()
+    {
+
+    }
+
+    public async Task RecordInvoicePaymentAsync()
+    {
+
+    }
+  }
+}
