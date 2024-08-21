1dae12d3625c945222482d6c3b6ab0df75cdefbc
Denys Olleik <dolleik@gmail.com>
2024-08-20 19:12:45 -0500

    Adding tenant table.

---
+++ Accounting/Controllers/DeploymentAndProvisioningController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  public class DeploymentAndProvisioningController : BaseController
+  {
+    public IActionResult Tenants()
+    {
+      return View();
+    }
+  }
+}
