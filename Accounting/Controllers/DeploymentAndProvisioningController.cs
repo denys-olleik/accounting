d1512d38f47d523fb61a20e185c1e21da6105ffb
Denys Olleik <dolleik@gmail.com>
2024-08-23 14:36:31 -0500

    Stub out some code.

--- Accounting/Controllers/DeploymentAndProvisioningController.cs
+++ Accounting/Controllers/DeploymentAndProvisioningController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Accounting.Models.TenantViewModels;
 
 namespace Accounting.Controllers
 {
+  [Route("dnp")]
   public class DeploymentAndProvisioningController : BaseController
   {
+    [Route("tenants")]
     public IActionResult Tenants()
     {
       return View();
+    }
+
+    [Route("create-tenant")]
+    [HttpGet]
+    public async Task<IActionResult> CreateTenant()
+    {
+      return View();
+    }
+
+    [Route("create-tenant")]
+    [HttpPost]
+    public async Task<IActionResult> CreateTenant(CreateTenantViewModel model)
+    {
+      throw new NotImplementedException();
     }
   }
 }
---
+++ Accounting/Models/TenantViewModels/CreateTenantViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation.Results;
+
+namespace Accounting.Models.TenantViewModels
+{
+  public class CreateTenantViewModel
+  {
+    public string? Email { get; set; }
+
+    public ValidationResult? ValidationResult { get; set; }
+  }
+}
