edb6f0eab4860e7221584713cb878a0a7da2cc81
Denys Olleik <dolleik@gmail.com>
2024-08-25 13:23:05 -0500

    Adding cloud type.

---
+++ Accounting.Business/Cloud.cs
@@ -0,0 +1,16 @@
+﻿using Accounting.Common;
+
+namespace Accounting.Business
+{
+  public class Cloud : IIdentifiable<int>
+  {
+    public int CloudID { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime Created { get; set; }
+    public int CreatedById { get; set; }
+    public int OrganizationId { get; set; }
+
+    public int Identifiable => this.CloudID;
+  }
+}
---
+++ Accounting.Database/Interfaces/ICloudManager.cs
@@ -0,0 +1,8 @@
+﻿using Accounting.Business;
+
+namespace Accounting.Database.Interfaces
+{
+  public interface ICloudManager : IGenericRepository<Cloud, int>
+  {
+  }
+}
