9d479e92bd372ae89af9151d99fb463bd973c252
DenysOlleik <dolleik@gmail.com>
2024-09-08 14:14:35 -0500

    ref

--- Accounting/Models/SecretViewModels/CreateSecretViewModel.cs
+++ Accounting/Models/SecretViewModels/CreateSecretViewModel.cs
@@ -8,6 +8,7 @@ namespace Accounting.Models.SecretViewModels
     public string? Value { get; set; }
     public string? Vendor { get; set; }
     public string? Purpose { get; set; }
+    public bool EncryptValue { get; set; }
 
     public ValidationResult? ValidationResult { get; set; }
   }
