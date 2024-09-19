a1b46c5bd0ac45c505b51a51f9d9d23fcd460a2b
DenysOlleik <dolleik@gmail.com>
2024-09-19 07:12:30 -0500

    adding props

--- Accounting.Common/ViewsConstants.cs
+++ Accounting.Common/ViewsConstants.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 namespace Accounting.Common
 {
-  internal class ViewsConstants
+  public class ViewsConstants
   {
+    public const string CreateProductOrService = "create-product-or-service";
+    public const string ReceivePayment = "receive-payment";
+
+    private static readonly List<string> _all = new List<string>();
+
+    static ViewsConstants()
+    {
+      var fields = typeof(ViewsConstants).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+      foreach (var field in fields)
+      {
+        if (field.FieldType == typeof(string) && field.GetValue(null) is string value)
+        {
+          _all.Add(value);
+        }
+      }
+    }
+
+    public static IReadOnlyList<string> All => _all.AsReadOnly();
   }
 }
