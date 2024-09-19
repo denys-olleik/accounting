44a9e011d0877c42956356a2f6a7a16c40a80679
DenysOlleik <dolleik@gmail.com>
2024-09-19 07:02:39 -0500

    Adding ViewsConstants. This will be used to save which accounts appear in which drop-downs in different views where an account has to be chosen.

---
+++ Accounting.Common/UnitTypeConstants.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace Accounting.Common
+{
+  public class UnitTypeConstants
+  {
+    public const string Piece = "piece";
+    public const string Kilogram = "kilogram";
+    public const string Meter = "meter";
+    public const string Liter = "liter";
+
+    private static readonly List<string> _all = new List<string>();
+
+    static UnitTypeConstants()
+    {
+      var fields = typeof(UnitTypeConstants).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+      foreach (var field in fields)
+      {
+        if (field.FieldType == typeof(string) && field.GetValue(null) is string value)
+        {
+          _all.Add(value);
+        }
+      }
+    }
+
+    public static IReadOnlyList<string> All => _all.AsReadOnly();
+  }
+}
