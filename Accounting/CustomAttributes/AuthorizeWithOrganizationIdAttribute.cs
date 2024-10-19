2814f81d6348c8a1c5d0d402fd7c8e4b7755bc64
DenysOlleik <dolleik@gmail.com>
2024-10-19 14:58:52 -0400

    ref

--- Accounting/CustomAttributes/AuthorizeWithOrganizationIdAttribute.cs
+++ Accounting/CustomAttributes/AuthorizeWithOrganizationIdAttribute.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System.Linq;
 using static Accounting.Business.Claim;
 
 namespace Accounting.CustomAttributes
