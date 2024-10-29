0371e7afff93a0d402e51a36d6d3b9b56de3edcc
DenysOlleik <dolleik@gmail.com>
2024-10-29 12:17:18 -0400

    Add Application Settings page and controller.

---
+++ Accounting/Controllers/ApplicationSettingController.cs
@@ -0,0 +1,23 @@
+﻿using Accounting.CustomAttributes;
+using Accounting.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [AuthorizeWithOrganizationId]
+  [Route("application-settings")]
+  public class ApplicationSettingController : BaseController
+  {
+    private readonly ApplicationSettingService _applicationSettingService;
+
+    public ApplicationSettingController(ApplicationSettingService applicationSettingService)
+    {
+      _applicationSettingService = applicationSettingService;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
