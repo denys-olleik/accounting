3336c715851b09bdb43f732e4533611e3b4a2203
DenysOlleik <dolleik@gmail.com>
2024-11-05 11:09:44 -0500

    ref

--- Accounting/Models/UserAccountViewModels/ChooseOrganizationViewModel.cs
+++ Accounting/Models/UserAccountViewModels/ChooseOrganizationViewModel.cs
@@ -2,17 +2,17 @@
 
 namespace Accounting.Models.UserAccountViewModels
 {
-  public class OrganizationViewModel
-  {
-    public int OrganizationId { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string? TenantPublicId { get; set; }
-  }
-
   public class ChooseOrganizationViewModel
   {
     public List<OrganizationViewModel> Organizations { get; set; } = null!;
     public int? SelectedOrganizationId { get; set; }
     public ValidationResult? ValidationResult { get; set; }
+  }
+
+  public class OrganizationViewModel
+  {
+    public int OrganizationId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? TenantPublicId { get; set; }
   }
 }
