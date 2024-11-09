a91f9caf26bcf1212482a4b8965caf2be54d1f70
DenysOlleik <dolleik@gmail.com>
2024-11-09 10:24:11 -0500

    ref

---
+++ Accounting/Validators/AddUserOrganizationViewModelValidator.cs
@@ -0,0 +1,23 @@
+﻿using Accounting.Models.Tenant;
+using Accounting.Service;
+using FluentValidation;
+
+namespace Accounting.Validators
+{
+  public class AddUserOrganizationViewModelValidator : AbstractValidator<AddUserOrganizationViewModel>
+  {
+    private readonly UserService _userService;
+    private readonly OrganizationService _organizationService;
+    private readonly string _tenantId;
+
+    public AddUserOrganizationViewModelValidator(
+      UserService userService,
+      OrganizationService organizationService,
+      string tenantId)
+    {
+      _userService = userService;
+      _organizationService = organizationService;
+      _tenantId = tenantId;
+    }
+  }
+}
---
+++ Accounting/Validators/TenantLoginViewModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using Accounting.Models.Tenant;
+using FluentValidation;
+
+namespace Accounting.Validators
+{
+  public class TenantLoginViewModelValidator
+    : AbstractValidator<TenantLoginViewModel>
+  {
+    public TenantLoginViewModelValidator()
+    {
+      RuleFor(x => x.Email)
+        .NotEmpty()
+        .WithMessage("Email is required.")
+        .DependentRules(() =>
+        {
+          RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password is required.");
+        });
+    }
+  }
+}
