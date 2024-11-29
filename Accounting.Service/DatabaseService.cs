aae8716ea879b64fa934550068d8e77d85ef7729
Denys Olleik <dolleik@gmail.com>
2024-11-29 03:50:43 -0500

    Refactor DatabaseService to remove _databaseName field.

--- Accounting.Service/DatabaseService.cs
+++ Accounting.Service/DatabaseService.cs
@@ -5,30 +5,28 @@ namespace Accounting.Service
 {
   public class DatabaseService
   {
-    private readonly string _databaseName = DatabaseThing.DatabaseConstants.Database; 
-
     public async Task ResetDatabase()
     {
-      var factoryManager = new FactoryManager(_databaseName);
+      var factoryManager = new FactoryManager(null);
       await factoryManager.GetDatabaseManager().ResetDatabaseAsync();
     }
 
     public async Task<DatabaseThing> CreateDatabaseAsync(string tenantId)
     {
-      var factoryManager = new FactoryManager(_databaseName);
+      var factoryManager = new FactoryManager(null);
       return await factoryManager.GetDatabaseManager().CreateDatabase(tenantId);
     }
 
-    public async Task RunSQLScript(string script)
+    public async Task RunSQLScript(string script, string databaseName)
     {
-      var factoryManager = new FactoryManager(_databaseName);
-      await factoryManager.GetDatabaseManager().RunSQLScript(script, _databaseName);
+      var factoryManager = new FactoryManager(null);
+      await factoryManager.GetDatabaseManager().RunSQLScript(script, databaseName);
     }
 
-    public async Task DeleteAsync()
+    public async Task DeleteAsync(string databaseName)
     {
-      var factoryManager = new FactoryManager(_databaseName);
-      await factoryManager.GetDatabaseManager().DeleteAsync(_databaseName);
+      var factoryManager = new FactoryManager(null);
+      await factoryManager.GetDatabaseManager().DeleteAsync(databaseName);
     }
   }
 }
