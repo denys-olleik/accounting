900e4ce9948fa851bf0f9844c5b8f1b5f5d176e7
Denys Olleik <dolleik@gmail.com>
2024-11-28 21:51:50 -0500

    Make databaseName optional in UserService constructor.

--- Accounting.Service/UserService.cs
+++ Accounting.Service/UserService.cs
@@ -7,7 +7,7 @@ namespace Accounting.Service
   {
     private readonly string _databaseName;
 
-    public UserService(string databaseName)
+    public UserService(string databaseName = null)
     {
       _databaseName = databaseName;
     }
