1b3485dbdcd7eed2d977bb036119e51721ece315
Denys Olleik <dolleik@gmail.com>
2024-12-30 11:28:19 -0500

    ref

---
+++ Accounting/Controllers/RegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using Accounting.Models.RegistrationViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [Authorize]
+  [Route("registration")]
+  public class RegistrationController : BaseController
+  {
+    [HttpGet]
+    [Route("register")]
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public IActionResult Register(RegisterViewModel model)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Accounting/Models/RegistrationViewModels/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation.Results;
+
+namespace Accounting.Models.RegistrationViewModels
+{
+  public class RegisterViewModel
+  {
+    public string Email { get; set; }
+
+    public ValidationResult ValidationResult { get; set; } = new();
+  }
+}
