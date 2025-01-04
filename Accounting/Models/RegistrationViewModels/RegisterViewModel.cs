6ee38eb5cc6e3a110985f82936171cec25c365a7
Denys Olleik <dolleik@gmail.com>
2025-01-04 16:41:36 -0500

    ref

--- Accounting/Models/RegistrationViewModels/RegisterViewModel.cs
+++ Accounting/Models/RegistrationViewModels/RegisterViewModel.cs
@@ -19,6 +19,9 @@ namespace Accounting.Models.RegistrationViewModels
       {
         RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Valid email is required");
         RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+
+        RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required");
+        RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required");
       }
     }
   }
