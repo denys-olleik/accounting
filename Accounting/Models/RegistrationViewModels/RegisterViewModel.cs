d06dfa63ee436755f0e4a2a84cf05e3c87099407
Denys Olleik <dolleik@gmail.com>
2025-03-31 13:02:31 -0400

    ref

--- Accounting/Models/RegistrationViewModels/RegisterViewModel.cs
+++ Accounting/Models/RegistrationViewModels/RegisterViewModel.cs
@@ -5,16 +5,64 @@ namespace Accounting.Models.RegistrationViewModels
 {
   public class RegisterViewModel
   {
-    public string? Email { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Password { get; set; }
-    public bool Shared { get; set; }
-    public string? FullyQualifiedDomainName { get; set; }
+    private string? _email;
+    private string? _firstName;
+    private string? _lastName;
+    private string? _password;
+    private string? _fullyQualifiedDomainName;
+    private string? _noReplyEmailAddress;
+    private string? _emailKey;
+    private string? _cloudKey;
 
-    public string? NoReplyEmailAddress { get; set; }
-    public string? EmailKey { get; set; }
-    public string? CloudKey { get; set; }
+    public string? Email
+    {
+      get => _email;
+      set => _email = value?.Trim();
+    }
+
+    public string? FirstName
+    {
+      get => _firstName;
+      set => _firstName = value?.Trim();
+    }
+
+    public string? LastName
+    {
+      get => _lastName;
+      set => _lastName = value?.Trim();
+    }
+
+    public string? Password
+    {
+      get => _password;
+      set => _password = value?.Trim();
+    }
+
+    public bool Shared { get; set; }
+
+    public string? FullyQualifiedDomainName
+    {
+      get => _fullyQualifiedDomainName;
+      set => _fullyQualifiedDomainName = value?.Trim();
+    }
+
+    public string? NoReplyEmailAddress
+    {
+      get => _noReplyEmailAddress;
+      set => _noReplyEmailAddress = value?.Trim();
+    }
+
+    public string? EmailKey
+    {
+      get => _emailKey;
+      set => _emailKey = value?.Trim();
+    }
+
+    public string? CloudKey
+    {
+      get => _cloudKey;
+      set => _cloudKey = value?.Trim();
+    }
 
     public ValidationResult ValidationResult { get; set; } = new();
 
