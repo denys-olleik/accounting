eae7dab1c4e0220e52cf82df4347a2e8b072b50e
Denys Olleik <dolleik@gmail.com>
2025-04-05 19:42:47 -0400

    ref

--- Accounting.Business/Blog.cs
+++ Accounting.Business/Blog.cs
@@ -5,6 +5,8 @@ namespace Accounting.Business
   public class Blog : IIdentifiable<int>
   {
     public int BlogID { get; set; }
+    public string? PublicId { get; set; }
+    public string? Slug { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
     public int CreatedById { get; set; }
