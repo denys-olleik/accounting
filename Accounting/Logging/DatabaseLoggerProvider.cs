80ce0e85a1e0eeabc80eef4e82c09a29ecd7b264
Denys Olleik <dolleik@gmail.com>
2025-04-19 06:32:23 -0400

    ref

---
+++ Accounting/Logging/DatabaseLoggerProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+public class DatabaseLoggerProvider : ILoggerProvider
+{
+  public ILogger CreateLogger(string categoryName)
+  {
+    return new DatabaseLogger(categoryName);
+  }
+
+  public void Dispose()
+  {
+    // Dispose resources if necessary
+  }
+}
