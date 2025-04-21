51c6ca9bb4d134a96ceba0ee0e87a164873077d4
Denys Olleik <dolleik@gmail.com>
2025-04-21 05:23:17 -0400

    ref

---
+++ Accounting/Controllers/RequestLogController.cs
@@ -0,0 +1,28 @@
+﻿using Accounting.CustomAttributes;
+using Accounting.Models.Item;
+using Accounting.Models.RequestLogViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [AuthorizeWithOrganizationId]
+  [Route("request-log")]
+  public class RequestLogController : BaseController
+  {
+    [HttpGet]
+    [Route("logs")]
+    public IActionResult Logs(int page = 1, int pageSize = 2)
+    {
+      var referer = Request.Headers["Referer"].ToString() ?? string.Empty;
+
+      var vm = new RequestLogsPaginatedViewModel
+      {
+        Page = page,
+        PageSize = pageSize,
+        RememberPageSize = string.IsNullOrEmpty(referer),
+      };
+
+      return View(vm);
+    }
+  }
+}
---
+++ Accounting/Models/RequestLogViewModels/RequestLogsPaginatedViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Accounting.Models.RequestLogViewModels
+{
+  public class RequestLogsPaginatedViewModel : PaginatedViewModel
+  {
+    public List<RequestLogViewModel>? RequestLogs { get; set; }
+
+    public class RequestLogViewModel
+    {
+      public string RequestLogID { get; set; }
+      public string RemoteIp { get; set; }
+      public string CountryCode { get; set; }
+      public string Referer { get; set; }
+      public string UserAgent { get; set; }
+      public string Path { get; set; }
+      public long ResponseLengthBytes { get; set; }
+      public string StatusCode { get; set; }
+      public DateTime Created { get; set; }
+    }
+  }
+}
