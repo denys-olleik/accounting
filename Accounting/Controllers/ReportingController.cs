3d8b79ccb3d2abe24e8861b207c65d7d820d9ef6
Denys Olleik <dolleik@gmail.com>
2025-04-22 19:59:34 -0400

    ref

--- Accounting/Controllers/ReportingController.cs
+++ Accounting/Controllers/ReportingController.cs
@@ -143,4 +143,46 @@ namespace Accounting.Controllers
       }
     }
   }
+
+  [AuthorizeWithOrganizationId]
+  [ApiController]
+  [Route("api/request-log")]
+  public class RequestLogApiController : BaseController
+  {
+    private readonly RequestLogService _requestLogService;
+
+    public RequestLogApiController(RequestContext requestContext, RequestLogService requestLogService)
+    {
+      _requestLogService = new RequestLogService(requestContext.DatabaseName, requestContext.DatabasePassword);
+    }
+
+    [HttpGet("get-request-logs")]
+    public async Task<IActionResult> GetRequestLogs(
+      int page = 1,
+      int pageSize = 10)
+    {
+      var (requestLogs, nextPage) = await _requestLogService.GetAllAsync(page, pageSize);
+
+      Models.RequestLogViewModels.GetRequestLogsViewModel getRequestLogsViewModel = new Models.RequestLogViewModels.GetRequestLogsViewModel()
+      {
+        RequestLogs = requestLogs.Select(log => new Models.RequestLogViewModels.GetRequestLogsViewModel.RequestLogViewModel()
+        {
+          RowNumber = log.RowNumber,
+          RequestLogID = log.RequestLogID,
+          RemoteIp = log.RemoteIp,
+          CountryCode = log.CountryCode,
+          Referer = log.Referer,
+          UserAgent = log.UserAgent,
+          Path = log.Path,
+          ResponseLengthBytes = log.ResponseLengthBytes,
+          StatusCode = log.StatusCode,
+          Created = log.Created
+        }).ToList(),
+        Page = page,
+        NextPage = nextPage
+      };
+
+      return Ok(getRequestLogsViewModel);
+    }
+  }
 }
