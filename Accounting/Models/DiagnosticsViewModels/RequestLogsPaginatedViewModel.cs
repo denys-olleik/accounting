27a0af771e2906e77c72d965e056bb173d7040f7
Denys Olleik <dolleik@gmail.com>
2025-04-22 21:17:49 -0400

    ref

--- Accounting/Models/DiagnosticsViewModels/RequestLogsPaginatedViewModel.cs
+++ Accounting/Models/DiagnosticsViewModels/RequestLogsPaginatedViewModel.cs
@@ -2,20 +2,6 @@
 {
   public class RequestLogsPaginatedViewModel : PaginatedViewModel
   {
-    public List<RequestLogViewModel>? RequestLogs { get; set; }
 
-    public class RequestLogViewModel
-    {
-      public int RowNumber { get; set; }
-      public string RequestLogID { get; set; }
-      public string RemoteIp { get; set; }
-      public string CountryCode { get; set; }
-      public string Referer { get; set; }
-      public string UserAgent { get; set; }
-      public string Path { get; set; }
-      public long ResponseLengthBytes { get; set; }
-      public string StatusCode { get; set; }
-      public DateTime Created { get; set; }
-    }
   }
 }
