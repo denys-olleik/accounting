5356bf250f1e40be814f4add0b3046a738cfdfbf
Denys Olleik <dolleik@gmail.com>
2025-04-22 21:10:04 -0400

    ref

--- Accounting/Models/RequestLogViewModels/ExceptionsPaginatedViewModel.cs
+++ Accounting/Models/RequestLogViewModels/ExceptionsPaginatedViewModel.cs
@@ -2,6 +2,19 @@
 {
   public class ExceptionsPaginatedViewModel : PaginatedViewModel
   {
-    public List<ExceptionLogViewModel>? Exceptions { get; set; }
+    public List<ExceptionViewModel>? Exceptions { get; set; }
+
+    public class ExceptionViewModel
+    {
+      public int ExceptionID { get; set; }
+      public string Message { get; set; }
+      public string StackTrace { get; set; }
+      public string Source { get; set; }
+      public int? HResult { get; set; }
+      public string TargetSite { get; set; }
+      public string InnerException { get; set; }
+      public int? RequestLogId { get; set; }
+      public DateTime Created { get; set; }
+    }
   }
 }
