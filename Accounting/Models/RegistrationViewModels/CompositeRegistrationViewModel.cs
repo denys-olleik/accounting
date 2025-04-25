dedff848ec743811d4e74c4fae6da4cfd0630d74
Denys Olleik <dolleik@gmail.com>
2025-04-25 07:19:44 -0400

    ref

--- Accounting/Models/RegistrationViewModels/CompositeRegistrationViewModel.cs
+++ Accounting/Models/RegistrationViewModels/CompositeRegistrationViewModel.cs
@@ -8,10 +8,11 @@ namespace Accounting.Models.RegistrationViewModels
   }
 
   public class CompositeRegistrationViewModel
-  {
+  { 
     public RegistrationType SelectedRegistrationType { get; set; }
     public SharedRegistrationViewModel Shared { get; set; } = new();
     public DedicatedRegistrationViewModel Dedicated { get; set; } = new();
     public IndependentRegistrationViewModel Independent { get; set; } = new();
+    public FluentValidation.Results.ValidationResult? ValidationResult { get; set; } = new();
   }
 }
