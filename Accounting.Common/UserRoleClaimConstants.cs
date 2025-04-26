fbe1a8eb323fa39992cb198600df0b568f836174
Denys Olleik <dolleik@gmail.com>
2025-04-26 09:16:54 -0400

    ref

---
+++ Accounting.Common/UserRoleClaimConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Common
+{
+  public static class UserRoleClaimConstants
+  {
+    public const string UserRole = "administrator";
+
+    private static readonly List<string> _all = new List<string>();
+
+    static UserRoleClaimConstants()
+    {
+      var fields = typeof(UserRoleClaimConstants).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+      foreach (var field in fields)
+      {
+        if (field.FieldType == typeof(string) && field.GetValue(null) is string value)
+        {
+          _all.Add(value);
+        }
+      }
+    }
+
+    public static IReadOnlyList<string> All => _all.AsReadOnly();
+  }
+}
