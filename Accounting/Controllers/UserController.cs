b8d0302983f356ccf70db77e41f0b5ef6747b7d5
Denys Olleik <dolleik@gmail.com>
2025-04-29 02:22:02 -0400

    ref

--- Accounting/Controllers/UserController.cs
+++ Accounting/Controllers/UserController.cs
@@ -1,15 +1,12 @@
 ﻿using Accounting.Business;
 using Accounting.Common;
 using Accounting.CustomAttributes;
-using Accounting.Models.TenantViewModels;
 using Accounting.Models.UserViewModels;
 using Accounting.Service;
-using Accounting.Validators;
 using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System.Transactions;
-using static Accounting.Business.Claim;
 
 namespace Accounting.Controllers
 {
@@ -94,63 +91,6 @@ namespace Accounting.Controllers
       return RedirectToAction("Users", new { page = 1, pageSize = 2 });
     }
 
-    //    using FluentValidation;
-    //using FluentValidation.Results;
-
-    //namespace Accounting.Models.UserViewModels
-    //  {
-    //    public class UpdateUserViewModel
-    //    {
-    //      private string? email;
-    //      private string? firstName;
-    //      private string? lastName;
-
-    //      public int UserID { get; set; }
-    //      public string? Email
-    //      {
-    //        get => email;
-    //        set => email = value?.Trim();
-    //      }
-
-    //      public string? FirstName
-    //      {
-    //        get => firstName;
-    //        set => firstName = value?.Trim();
-    //      }
-
-    //      public string? LastName
-    //      {
-    //        get => lastName;
-    //        set => lastName = value?.Trim();
-    //      }
-
-    //      public List<OrganizationViewModel> AvailableOrganizations { get; set; } = new();
-    //      public List<string> AvailableRoles { get; set; } = new();
-    //      public List<string> SelectedRoles { get; set; } = new();
-    //      public string? SelectedOrganizationIdsCsv { get; set; }
-
-    //      public int CurrentRequestingUserId { get; set; }
-    //      public ValidationResult ValidationResult { get; set; } = new();
-
-    //      public class OrganizationViewModel
-    //      {
-    //        public int OrganizationID { get; set; }
-    //        public string? Name { get; set; }
-    //      }
-
-    //      public class UpdateUserViewModelValidator : AbstractValidator<UpdateUserViewModel>
-    //      {
-    //        public UpdateUserViewModelValidator()
-    //        {
-    //          RuleFor(x => x.UserID).GreaterThan(0);
-    //          RuleFor(x => x.Email).EmailAddress().NotEmpty();
-    //          RuleFor(x => x.FirstName).NotEmpty();
-    //          RuleFor(x => x.LastName).NotEmpty();
-    //        }
-    //      }
-    //    }
-    //  }
-
     [HttpGet]
     [Route("update/{userId}")]
     public async Task<IActionResult> UpdateUser(int userId)
@@ -175,7 +115,10 @@ namespace Accounting.Controllers
           OrganizationID = x.OrganizationID,
           Name = x.Name
         }).ToList(),
-        AvailableRoles = CustomClaimTypeConstants.All.ToList(),
+        AvailableRoles = new List<string>()
+        {
+          UserRoleClaimConstants.TenantManager,
+        },
         SelectedOrganizationIdsCsv = string.Join(',', userOrganizations.Select(x => x.OrganizationID)),
         CurrentRequestingUserId = GetUserId()
       });
